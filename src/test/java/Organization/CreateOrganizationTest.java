a9d2f804ab2221a595799b0f18f1ca4d0665a09b
Shanthi M <Shanthi M@LAPTOP-D7GO7M62>
2025-12-29 11:00:42 +0530

    edited orgn

--- src/test/java/Organization/CreateOrganizationTest.java
+++ src/test/java/Organization/CreateOrganizationTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class CreateOrganizationTest {
 @Test
 public void createorg() {
-	System.out.println("created");
+	System.out.println("created123");
 	System.out.println("created");
 	System.out.println("created");
 	

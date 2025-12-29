5f7990b8faa8d5ae385c7cc8271f9cd5c9117611
hp <hp@DESKTOP-DJ28UVR>
2025-12-29 13:35:45 +0530

    fork

--- src/test/java/contacts_Module/editContactTest.java
+++ src/test/java/contacts_Module/editContactTest.java
@@ -3,10 +3,8 @@ package contacts_Module;
 public class editContactTest {
 	public static void main(String[] args) {
 		System.out.println("edit");
-			System.out.println("edit");
+	
 		
 	}
-		
-	
 
 }

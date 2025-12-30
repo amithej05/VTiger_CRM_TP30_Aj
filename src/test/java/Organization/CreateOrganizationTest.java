5681d00daf416c057c45f4ce23f9975a70a72298
kavyaevangeline <hpprobookmmo@gmail.com>
2025-12-30 14:38:32 +0530

    Update CreateOrganizationTest.java

--- src/test/java/Organization/CreateOrganizationTest.java
+++ src/test/java/Organization/CreateOrganizationTest.java
@@ -1,16 +1,1 @@
-package Organization;
 
-import org.testng.annotations.Test;
-
-public class CreateOrganizationTest {
-@Test
-public void createorg() {
-	System.out.println("created123");
-	System.out.println("created");
-	System.out.println("created");
-	
-	System.out.println("created");
-	
-}
-
-}
